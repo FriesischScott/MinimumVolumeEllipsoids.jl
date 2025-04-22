52552cf70d8fd1ad7e9b54362bb0a82e30129934
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2025-04-22 18:07:14 +0200

    Add more tests

--- test/minimumvolumeellipsoid.jl
+++ test/minimumvolumeellipsoid.jl
@@ -1,20 +1,62 @@
 @testset "Minimum Volume Ellipsoid" begin
-    X = [
-        -1 -1 1 2
-        1 -1 -1 2
-    ]
-
     @testset "Centered" begin
+        X = [
+            -1 -1 1 2
+            1 -1 -1 2
+        ]
         ϵ = minimum_volume_ellipsoid(X, 1e-10; centered=true)
 
-        @test ϵ.x == [0, 0]
+        @test ϵ.c == [0, 0]
         @test ϵ.H ≈ [5/8 -3/8; -3/8 5/8] atol = 1e-10
+
+        for x in eachcol(X)
+            @test x in ϵ
+        end
     end
 
     @testset "Not Centered" begin
+        X = [
+            -1 -1 1 2
+            1 -1 -1 2
+        ]
         ϵ = minimum_volume_ellipsoid(X, 1e-10)
 
-        @test ϵ.x ≈ [0.5, 0.5] atol = 1e-10
+        @test ϵ.c ≈ [0.5, 0.5] atol = 1e-10
         @test ϵ.H ≈ [2/3 -2/9; -2/9 2/3] atol = 1e-10
+
+        for x in eachcol(X)
+            @test x in ϵ
+        end
+    end
+
+    @testset "Datasets" begin
+        @testset "Iris" begin
+            using RDatasets
+
+            iris = dataset("datasets", "iris")
+
+            X = permutedims([iris.SepalLength iris.SepalWidth])
+
+            ϵ = minimum_volume_ellipsoid(X, 1e-10)
+
+            for x in eachcol(X)
+                @test x in ϵ
+            end
+        end
+
+    end
+
+    @testset "rand" begin
+        X = [
+            -1 -1 1 2
+            1 -1 -1 2
+        ]
+        ϵ = minimum_volume_ellipsoid(X, 1e-10)
+
+        U = rand(ϵ, 10^4)
+
+        for x in eachcol(U)
+            @test x in ϵ
+        end
     end
 end
